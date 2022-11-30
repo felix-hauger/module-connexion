1880245be73b2428975f916da9d44fae3f2d0617
felix-hauger <felix.hauger@laplateforme.io>
2022-11-30 10:04:52 +0100

    connect.php - checked if login exists in db & if the password is correct

--- functions/connect.php
+++ functions/connect.php
@@ -2,10 +2,11 @@
 
 $id = new mysqli('localhost', 'root', '', 'moduleconnexion');
 
-if ($id->query($sql)) {
-    echo 'Action complete! ';
-} else {
-    echo 'Error: ' . $id->error;
-}
+$query = $id->query($sql);
+// if ($id->query($sql)) {
+//     echo 'Action complete! ';
+// } else {
+//     echo 'Error: ' . $id->error;
+// }
 
 // mysqli_close($id);
--- login.php
+++ login.php
@@ -1,5 +1,48 @@
 <?php
 
+if (!empty($_POST['login']) && !empty($_POST['password'])) {
+    $input_login = $_POST['login'];
+    $input_password = $_POST['password'];
+
+}
+
+$login = 'admin';
+$password = 'admin';
+
+$sql = "SELECT login FROM users";
+
+require('functions/connect.php');
+
+$row = $query->fetch_assoc();
+
+while ($row != null) {
+    // var_dump($row);
+    foreach ($row as $db_login) {
+        // var_dump($key);
+        // var_dump($login);
+        if ($login === $db_login) {
+            echo 'le login ' . $login . ' est dans la base de données<br>';
+            
+            $sql = "SELECT login, password FROM users WHERE login LIKE '$login'";
+
+            require('functions/connect.php');
+
+            $row_user_login_password = $query->fetch_assoc();
+
+            if ($row_user_login_password['password'] === $password) {
+                echo 'utilisateur ' . $login . ' connecté !';
+            }
+            var_dump($row_user_login_password);
+
+        } else {
+            // echo 'le login ' . $login . ' n\'est pas dans la base de données<br>';
+            
+        }
+    }
+    $row = $query->fetch_assoc();
+
+}
+
 
 ?>
 
