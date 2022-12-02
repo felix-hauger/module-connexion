189077d92680602b63cb76feb24c1afe93ed8a13
felix-hauger <felix.hauger@laplateforme.io>
2022-12-02 19:56:05 +0100

    admin.php - display users infos & redirect to log in page if not admin

---
+++ admin.php
@@ -0,0 +1,50 @@
+<?php
+session_start();
+
+require_once('functions/connect.php');
+require_once('functions/is_user_in_db.php');
+
+var_dump($_SESSION);
+
+if ($_SESSION['logged_user'] != 'admin') {
+    header('Location: connexion.php');
+}
+
+$sql = "SELECT id, firstname, lastname, login, password FROM users";
+
+$query = $id->query($sql);
+
+$row = $query->fetch_assoc();
+?>
+
+<table>
+    <thead>
+        <tr>
+            <?php
+            echo '<table><thead><tr>';
+
+            foreach ($row as $key => $value) {
+                echo '<th>' . $key . '</th>';
+            }
+
+            echo '</tr></thead><tbody>';
+            ?>
+        </tr>
+    </thead>
+    <tbody>
+        <?php
+        while ($row != null) {
+
+            echo '<tr>';
+
+            foreach ($row as $value) {
+                echo '<td>' . $value . '</td>';
+            }
+
+            echo '</tr>';
+
+            $row = $query->fetch_assoc();
+        }
+        ?>
+    </tbody>
+</table>
